278d83154b9f9ad5303566a30e25c70daf010b33
steffan96 <stefanmilicic@yahoo.com>
2024-05-08 09:43:05 +0200

    07.05 - creating tokens

--- internal/data/models.go
+++ internal/data/models.go
@@ -20,12 +20,14 @@ type Models struct {
 	// }
 	Movies MovieModel
 	Users  UserModel
+	Tokens TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
 		Users:  UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
 
