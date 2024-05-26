4078a8b61419809c9edca2916f57b547562d91bf
steffan96 <stefanmilicic@yahoo.com>
2024-05-26 18:32:23 +0200

    26.05 - metrics

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -30,5 +30,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
-	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
